54ef648967d87b35e63cf494089e9f40253b955b
Neeraj R <violinistneeraj@gmail.com>
2022-01-27 12:11:32 +0530

    Configuring with Mail Catcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,16 +68,14 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.raise_delivery_errors = true
+  # Mail Catcher Config
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "neerajwebreply@gmail.com",
-    :password => "GMAIL_PASSWORD",
-    :authentication => 'plain',
-    :enable_starttls_auto => true
+    :address => "localhost",
+    :port => 1025,
   }
 
 
